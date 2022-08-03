34b5a17e0d68a77771b137d4e5168f20b52b0646
SzakacsAdam <szakacs.adam.98@gmail.com>
2022-08-04 01:25:35 +0200

    ADD: mimetype

--- src/pdf_handler/__init__.py
+++ src/pdf_handler/__init__.py
@@ -1,4 +1,5 @@
 from .pdf_container import PdfContainer
 from .pdf_convert_type import PdfConvertType
+from .pdf_converted_mime_type import PdfConvertedMimeType
 from .pdf_file import PdfFile
 from .pdf_status import PdfStatus
