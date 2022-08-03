12c9470aa174c85e0dd0923fbbe3131e6ca21901
SzakacsAdam <szakacs.adam.98@gmail.com>
2022-08-03 17:51:29 +0200

    CREATE: class for config reader and holder

--- src/config_reader/config_reader.py
+++ src/config_reader/config_reader.py
@@ -1,7 +1,22 @@
+from configparser import ConfigParser
+from os import getcwd
+from os.path import join
+
+from .config_example import config_example
+
+
 class ConfigReader:
-    config: dict[str, str | int] = dict()
+    config_filename: str = "config.ini"
+    example_config_filename: str = "config_example.ini"
+    config: ConfigParser = ConfigParser()
 
-    __slots__ = "config_path"
+    def __init__(self):
+        self.cwd: str = getcwd()
 
-    def __init__(self, config_path: str):
-        self.config_path: str = config_path
+    def create_example_config(self) -> None:
+        conf_path: str = join(self.cwd, self.example_config_filename)
+        try:
+            with open(conf_path, "w+") as conf_file:
+                conf_file.write(config_example)
+        except OSError as err:
+            return
